6fdd359a856e181705916b38d6ee7848cccd3390
Simon MacDonald <simon.macdonald@gmail.com>
2018-05-28 20:09:24 -0400

    :tada: First pass at implementation

---
+++ index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const chalk = require('chalk');
+
+const currentNodeVersion = process.versions.node;
+const semver = currentNodeVersion.split('.');
+const major = semver[0];
+
+if (major < 6) {
+  console.error(
+    chalk.red(
+      'You are running Node ' +
+        currentNodeVersion +
+        '.\n' +
+        'Create Whisk Action requires Node 6 or higher. \n' +
+        'Please update your version of Node.'
+    )
+  );
+  process.exit(1);
+}
+
+require('./createWhiskAction');
